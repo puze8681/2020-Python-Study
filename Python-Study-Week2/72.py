0de7d2ca9f7fb3cd8f22c9fa7e1992a326df701d
puze8681 <puze8681@gmail.com>
2020-07-12 03:31:39 +0900

    Week2: complete 72

---
+++ Python-Study-Week2/72.py
@@ -0,0 +1,10 @@
+word = input('문자를 입력하세요. ')
+isPalindrome = True
+for i in (0, len(word) -1):
+    if word[i] != word[len(word)-i-1]:
+        print("Palindrome 이 아닙니다.")
+        isPalindrome = False
+        break;
+
+if isPalindrome:
+    print("Palindrome 이 맞습니다.")

c4452cb5eebcbeb8f88a9cc1445fe68dde1c4e54
puze8681 <puze8681@gmail.com>
2020-07-17 16:02:13 +0900

    Week3: complete 89

---
+++ Python-Study-Week3/89.py
@@ -0,0 +1,28 @@
+def capitalize(original):
+    result = ""
+    punctuations = '''!,.?'''
+    isEnd = True
+    print(punctuations)
+    for i in range(len(original)):
+        c = original[i]
+        if c in punctuations:
+            isEnd = True
+            result = result + c
+        else:
+            if isEnd and c.isalpha():
+                result = result + c.upper()
+                isEnd = False
+            else:
+                if c == 'i' and i != 0:
+                    print(c, i)
+                    if original[i-1] == original[i+1] == ' ':
+                        result = result + c.upper()
+                    else:
+                        result = result + c
+                else:
+                    result = result + c
+    print(original)
+    print(result)
+
+
+capitalize(input('문자열을 입력하세요. '))

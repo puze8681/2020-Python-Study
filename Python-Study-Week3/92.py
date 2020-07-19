0e65cea3cc2cdd460f8b49029ff65372b6e12cf2
puze8681 <puze8681@gmail.com>
2020-07-19 20:47:07 +0900

    Week3: complete 92

---
+++ Python-Study-Week3/92.py
@@ -0,0 +1,12 @@
+def prime(number):
+    isPrime = True
+    if number > 1:
+        for i in range(2, number):
+            if number % i == 0:
+                isPrime = False
+        print(isPrime)
+    else:
+        print("1보다 큰 수를 입력해주세요.")
+
+
+prime(int(input('문자열을 입력하세요. ')))

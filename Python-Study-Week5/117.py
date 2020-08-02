a79bf1802a2fdbfc0c797979cef7e5530515ac55
puze8681 <puze8681@gmail.com>
2020-08-02 16:49:31 +0900

    Week5: clear 117

--- Python-Study-Week5/117.py
+++ Python-Study-Week5/117.py
@@ -1,26 +1,76 @@
+import decimal
+
+# xy - 시그마 x * 시그마 y
+# (시그마 x제곱 * n) - (시그마 x)제곱
+
+
+def sigma(sigmaList):
+    sum = 0
+    for data in sigmaList:
+        sum += data
+    return sum
+
+
+def sigma2(sigmaList1, sigmaList2):
+    sum = 0
+    for i in range(0, len(sigmaList1)):
+        sum += (sigmaList1[i] * sigmaList2[i])
+    return sum
+
+
+def sigma3(sigmaList):
+    sum = 0
+    for data in sigmaList:
+        sum += (data ** 2)
+    return sum
+
+
+def getList(dataList, index):
+    returnList = []
+    for data in dataList:
+        returnList.append(data[index])
+    return returnList
+
+
 pointCount = 0
 pointList = []
 totalDistance = 0
 while 1:
     if pointCount == 0:
-        x = float(input('Enter the x part of the coordinate: '))
-        y = float(input('Enter the y part of the coordinate: '))
+        x = decimal.Decimal(input('Enter the x part of the coordinate: '))
+        y = decimal.Decimal(input('Enter the y part of the coordinate: '))
         pointList.append((x, y))
         pointCount += 1
     else:
         x = input('Enter the x part of the coordinate: (blank to quit):')
         if x == "":
-            # m 은 기울기, b 는 상수
-            m = 0
-            b = 0
-            for i in range(0, len(pointList)-1):
-                # 각 좌표를 통해 가장 최적의 기울기와 상수를 구해야 함
-                m += 1
-                b += 1
-            print("y={}x+{}".format(m, b))
+            n = len(pointList)
+            xList = getList(pointList, 0)
+            yList = getList(pointList, 1)
+            sigmaX = sigma(xList)
+            sigmaY = sigma(xList)
+            sigmaXY = sigma2(xList, yList)
+            sigmaX2 = sigma3(xList)
+
+            m = (sigmaXY - ((sigmaX * sigmaY) / n)) / (sigmaX2 - ((sigmaX ** 2) / n))
+            b = (sigmaY / n) - (m * (sigmaX / n))
+            isEndWith0 = False
+            while not isEndWith0:
+                b = str(b)
+                if '.' in b:
+                    if b[len(b) - 1] == '.' or b[len(b)-1] == '0':
+                        b = list(b)
+                        del b[len(b) - 1]
+                        b = ''.join(b)
+                    else:
+                        isEndWith0 = True
+                else:
+                    isEndWith0 = True
+            b = decimal.Decimal(b)
+            print("y = {}x + {}".format(m, b))
             break
         else:
-            x = float(x)
-            y = float(input('Enter the y part of the coordinate: '))
+            x = decimal.Decimal(x)
+            y = decimal.Decimal(input('Enter the y part of the coordinate: '))
             pointList.append((x, y))
             pointCount += 1

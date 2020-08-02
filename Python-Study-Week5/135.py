3ead6d74f18ccc6a2d6070d882a51ef7e25f1122
puze8681 <puze8681@gmail.com>
2020-08-02 15:46:12 +0900

    Week5: clear 135

---
+++ Python-Study-Week5/135.py
@@ -0,0 +1,23 @@
+string1 = input('문자1 을 입력해주세요. ')
+string2 = input('문자2 을 입력해주세요. ')
+
+strList1 = []
+strList2 = []
+
+for c in string1:
+    if c.isalpha():
+        strList1.append(c.upper())
+
+for c in string2:
+    if c.isalpha():
+        strList2.append(c.upper())
+
+strList1.sort()
+strList2.sort()
+
+if strList1 == strList2:
+    print("아나그램입니다.")
+else:
+    print("아나그램이 아닙니다.")
+print(strList1)
+print(strList2)
